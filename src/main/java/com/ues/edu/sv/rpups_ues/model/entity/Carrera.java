985859116eaafab157aa068e4f5203f19138ceba
Oscar Adolfo Paises <41811599+pb16016@users.noreply.github.com>
2025-08-27 19:33:31 -0600

    Correción de codigo carrera

--- src/main/java/com/ues/edu/sv/rpups_ues/model/entity/Carrera.java
+++ src/main/java/com/ues/edu/sv/rpups_ues/model/entity/Carrera.java
@@ -15,9 +15,9 @@ import java.io.Serializable;
 public class Carrera implements Serializable {
 
     @Id
-    @Size(max = 10, message = "El código no puede tener más de 10 caracteres.")
+    @Size(max = 7, message = "El código no puede tener más de 7 caracteres.")
     @NotBlank(message = "El código no puede estar vacío.")
-    @Column(name = "codigo", length = 10, nullable = false, unique = true)
+    @Column(name = "codigo", length = 7, nullable = false, unique = true)
     private String codigo;
 
     @Size(max = 250, message = "El nombre de la carrera no puede tener más de 250 caracteres.")
